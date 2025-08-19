0f0d0580c48c87c782cb9cd75bd924eac0dcc778
ektto1041 <dhkdwk1041@gmail.com>
2025-08-19 16:16:17 +0900

    chore: karpotic/src/types 추가

---
+++ packages/karpotic/src/types/common.ts
@@ -0,0 +1,5 @@
+export type SizeWithoutX = "sm" | "md" | "lg";
+
+export type Size = SizeWithoutX | "xs" | "xl";
+
+export type Color = "default" | "dark";
---
+++ packages/karpotic/src/types/index.ts
@@ -0,0 +1,1 @@
+export { Color, Size, SizeWithoutX } from "./common";
