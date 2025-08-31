b20b090309b2854656770414c1dd0d93661afabe
ektto1041 <dhkdwk1041@gmail.com>
2025-08-31 15:01:13 +0900

    chore: parseDimension/Spacing 추가

---
+++ packages/karpotic/src/utils/utils.ts
@@ -0,0 +1,127 @@
+import { DimensionValue, SpacingValue } from "../types";
+
+export const parseDimension = (dimension?: string | DimensionValue) => {
+  if (!dimension) return {};
+
+  if (typeof dimension === "string") {
+    return { value: dimension };
+  }
+
+  const [min, value, max] = dimension;
+  return {
+    min,
+    value,
+    max,
+  };
+};
+
+export const parseSpacing = (spacing?: SpacingValue) => {
+  if (!spacing) return {};
+
+  if (typeof spacing === "string") {
+    return {
+      paddingTop: spacing,
+      paddingRight: spacing,
+      paddingBottom: spacing,
+      paddingLeft: spacing,
+    };
+  }
+
+  const [first, second, third, fourth] = spacing;
+
+  if (spacing.length === 2) {
+    return {
+      paddingTop: first,
+      paddingRight: second,
+      paddingBottom: first,
+      paddingLeft: second,
+    };
+  }
+
+  if (spacing.length === 3) {
+    return {
+      paddingTop: first,
+      paddingRight: second,
+      paddingBottom: third,
+      paddingLeft: second,
+    };
+  }
+
+  if (spacing.length === 4) {
+    return {
+      paddingTop: first,
+      paddingRight: second,
+      paddingBottom: third,
+      paddingLeft: fourth,
+    };
+  }
+
+  return {};
+};
+
+export const parseMargin = (spacing?: SpacingValue) => {
+  if (!spacing) return {};
+
+  if (typeof spacing === "string") {
+    return {
+      marginTop: spacing,
+      marginRight: spacing,
+      marginBottom: spacing,
+      marginLeft: spacing,
+    };
+  }
+
+  const [first, second, third, fourth] = spacing;
+
+  if (spacing.length === 2) {
+    return {
+      marginTop: first,
+      marginRight: second,
+      marginBottom: first,
+      marginLeft: second,
+    };
+  }
+
+  if (spacing.length === 3) {
+    return {
+      marginTop: first,
+      marginRight: second,
+      marginBottom: third,
+      marginLeft: second,
+    };
+  }
+
+  if (spacing.length === 4) {
+    return {
+      marginTop: first,
+      marginRight: second,
+      marginBottom: third,
+      marginLeft: fourth,
+    };
+  }
+
+  return {};
+};
+
+export const createStyleFromDimensions = (
+  w?: string | DimensionValue,
+  h?: string | DimensionValue,
+  p?: SpacingValue,
+  m?: SpacingValue
+) => {
+  const width = parseDimension(w);
+  const height = parseDimension(h);
+  const padding = parseSpacing(p);
+  const margin = parseMargin(m);
+
+  return {
+    width: width.value,
+    height: height.value,
+    minWidth: width.min,
+    maxWidth: width.max,
+    minHeight: height.min,
+    maxHeight: height.max,
+    ...padding,
+    ...margin,
+  };
+};
