39d7d9a6b6a4fcad48987bdf2e24ba2d66166779
ektto1041 <dhkdwk1041@gmail.com>
2025-09-08 16:05:49 +0900

    refactor: Line Container 기반 컴포넌트로 수정

--- packages/karpotic/src/Line/Line.tsx
+++ packages/karpotic/src/Line/Line.tsx
@@ -1,38 +1,26 @@
 import { c } from "@karponents-ui/utils";
 import { FC } from "react";
-import {
-  createStyleFromBackgroundColor,
-  createStyleFromDimensions,
-  createStylesFromFlex,
-} from "../utils";
+import { Container } from "../Container";
+import { createStylesFromFlex } from "../utils";
 import { LineProps } from "./Line.types";
 import styles from "./style.module.css";
 
 const Line: FC<LineProps> = ({
-  bgc,
-  w,
-  h,
-  p,
-  m,
   direction = "row",
   multiline,
   gap,
   jc,
   ai,
   ac,
+  className,
   style,
   children,
-  className,
   ...restProps
 }) => {
-  const dimensionStyles = createStyleFromDimensions(w, h, p, m);
-  const bgcStyle = createStyleFromBackgroundColor(bgc);
   const flexStyles = createStylesFromFlex(direction, gap, jc, ai, ac);
-  
-  const combinedStyles = {
+
+  const combinedStyle = {
     ...style,
-    ...dimensionStyles,
-    ...bgcStyle,
     ...flexStyles,
   };
 
@@ -43,13 +31,13 @@ const Line: FC<LineProps> = ({
   );
 
   return (
-    <div
+    <Container
       {...restProps}
-      style={combinedStyles}
+      style={combinedStyle}
       className={composedClassName}
     >
       {children}
-    </div>
+    </Container>
   );
 };
 
